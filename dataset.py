a8a22d5b9af6ee80db492e88f4df0bb24c851c29
Himer65 <112268377+Himer65@users.noreply.github.com>
2023-12-02 14:04:33 +0700

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -9,7 +9,7 @@ class Flickr8k(Dataset):
                  device: str = "cpu"):
         data = torch.load(path)
         self.x = data["x"].to(device) / 127.5 - 1
-        self.y = data["y"].to(device)
+        self.y = data["y"].to(device)[:, :, 0]
 
     def __len__(self):
         return self.x.shape[0]
@@ -17,8 +17,6 @@ class Flickr8k(Dataset):
     def __getitem__(self, key: int) -> tuple[torch.Tensor, torch.Tensor]:
         x = self.x[key]
         y = self.y[:, key]
-        rand = randint(0, y.shape[-1] - 1)
-        y = y[:, rand]
 
         return (x, y)
 
