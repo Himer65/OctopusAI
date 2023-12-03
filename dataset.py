179a0805b7b856ef6509068507da979448f7edc4
Himer65 <112268377+Himer65@users.noreply.github.com>
2023-12-03 13:55:17 +0700

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -7,14 +7,14 @@ class Flickr8k(Dataset):
                  path: str,
                  device: str = "cpu"):
         data = torch.load(path)
-        self.x = data["x"].to(device) / 127.5 - 1
+        self.x = data["x"].to(device)
         self.y = data["y"].to(device)[:, :, 0]
 
     def __len__(self):
         return self.x.shape[0]
 
     def __getitem__(self, key: int) -> tuple[torch.Tensor, torch.Tensor]:
-        x = self.x[key]
+        x = self.x[key] / 127.5 - 1
         y = self.y[:, key]
 
         return (x, y)
