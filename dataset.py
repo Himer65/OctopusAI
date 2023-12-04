2329cbedecec84b81ae8e90cc1bb0b7ba0866c3a
Himer65 <nurikmeskov@gmail.com>
2023-12-04 19:04:46 +0700

    fix dataset

--- dataset.py
+++ dataset.py
@@ -6,19 +6,18 @@ class Flickr8k(Dataset):
     def __init__(self,
                  path: str,
                  device: str = "cpu"):
-        data = torch.load(path)
-        self.x = data["x"].to(device)
-        self.y = data["y"].to(device)
+        data = torch.load(path, map_location=device)
+        self.x = data["x"]
+        self.y = data["y"]
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self.x.shape[0]
 
     def __getitem__(self, key: int) -> tuple[torch.Tensor, torch.Tensor]:
         x = self.x[key] / 127.5 - 1
-        y = self.y[:, key]
+        y = self.y[key]
 
         return (x, y)
-
 
 if __name__ != "__main__":
     dataset = Flickr8k(
